54d7472353a25c42d4b24c6a48013f1dad095db6
Nahuel Chawi Rojas <nadaro@outlook.com>
2023-01-23 19:45:08 -0300

    nueva entrega clase03

--- Assets/Scripts/cilinder.cs
+++ Assets/Scripts/cilinder.cs
@@ -12,30 +12,37 @@ public class Cilinder : MonoBehaviour
     [Tooltip("Vida de 0 100")]
     [Range(0, 100)]
     private float life;
+    [SerializeField]
+
 
 
    
     void Update()
     {
-    Cure();
-    var buttonW = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
-    var buttonA = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow);
-    var buttonS = Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
-    var buttonD = Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow);
-    var speedDelta = speed * Time.deltaTime;
+        Mov();
+        if(life <= 50)Cure();
+        else Damage();
+    }
+    void Cure(){
+        life += 0.004f;
+    }
+    void Damage(){
+        life -= 0.002f;
+    }
+    void Mov(){
+        var buttonW = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+        var buttonA = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow);
+        var buttonS = Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
+        var buttonD = Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow);
+        var speedDelta = speed * Time.deltaTime;
 
         if (buttonW) Speed(0, 0, speedDelta);//arriba , eje Z
         if (buttonS) Speed(0, 0, -speedDelta);//abajo, eje -Z
         if (buttonA) Speed(-speedDelta, 0, 0);//izquierda, eje -X
         if (buttonD) Speed(speedDelta, 0,0);//derecha, eje X
     }
-    void Speed(float x,float y,float z)
-    {
+    void Speed(float x,float y,float z){
         transform.Translate(x, y, z);
-    }
-    void Cure()
-    {
-        if(life <100)life+= 0.05f;
     }
    
 }
